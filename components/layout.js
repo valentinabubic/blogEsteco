5519301c8ff7a82f31488831a142da73ce139142
WhiteLion0950 <valentina95bubic@gmail.com>
2021-05-19 12:36:29 +0200

    spazio dopo bio autore nei post

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,3 @@
-
 import Head from "next/head";
 import Link from "next/link";
 
@@ -9,35 +8,40 @@ export default function Layout({ children, home, backUrl }) {
   return (
     <div className="soul-content soul-font-scale-l">
       <div className="soul-grid  soul-grid--horizontal  soul-grid--justify-center">
-      <div class="soul-grid-item-1/2-span"><div><Head></Head>
-        <header>
-          {home ? (
-            <>
-              <h1>{name}</h1>
-            </>
-          ) : (
-            <>
-              <h2>
-                <Link href="/blog">
-                  <a>{name}</a>
-                </Link>
-              </h2>
-            </>
-          )}
-        </header>
-        <main >{children}</main>
-        {backUrl ? (
-          <>
-            <div className="a-button  a-button--primary">
-              <Link href={backUrl}>
-                <a>← Home</a>
-              </Link>
-            </div>
-          </>
-        ) : (
-          <></>
-        )}</div></div>
-        
+        <div class="soul-grid-item-1/2-span">
+          <div>
+            <Head></Head>
+            <header>
+              {home ? (
+                <>
+                  <h1>{name}</h1>
+                </>
+              ) : (
+                <>
+                  <h2>
+                    <Link href="/blog">
+                      <a>{name}</a>
+                    </Link>
+                  </h2>
+                </>
+              )}
+            </header>
+            <main>{children}</main>
+            <br></br>
+            <br></br>
+            {backUrl ? (
+              <>
+                <div className="a-button  a-button--primary">
+                  <Link href={backUrl}>
+                    <a>← Home</a>
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
